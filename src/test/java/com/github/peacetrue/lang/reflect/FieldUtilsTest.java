953299be4ef4b2abce58e231264be5f47bc8e644
peacetrue <xiayouxue@gmail.com>
2022-11-08 20:25:36 +0800

    实现 Bean 遍历

--- src/test/java/com/github/peacetrue/lang/reflect/FieldUtilsTest.java
+++ src/test/java/com/github/peacetrue/lang/reflect/FieldUtilsTest.java
@@ -8,21 +8,28 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author peace
  **/
 class FieldUtilsTest {
 
+    private static List<Field> exclude(Stream<Field> fields) {
+        return fields
+                .filter(field -> !field.getName().startsWith("$jacoco"))
+                .collect(Collectors.toList());
+    }
+
     @Test
     void getDeclaredFields() {
-        List<Field> fields = JacocoUtils.exclude(FieldUtils.getDeclaredFields(User.class).stream());
+        List<Field> fields = exclude(FieldUtils.getDeclaredFields(User.class).stream());
         Assertions.assertEquals(
                 Arrays.asList("id", "name", "age", "nation"),
                 fields.stream().map(Field::getName).collect(Collectors.toList())
         );
 
-        List<Field> fields2 = JacocoUtils.exclude(FieldUtils.getDeclaredFields(EmployeeUser.class).stream());
+        List<Field> fields2 = exclude(FieldUtils.getDeclaredFields(EmployeeUser.class).stream());
         Assertions.assertEquals(
                 Arrays.asList("id", "name", "age", "nation", "job", "pay"),
                 fields2.stream().map(Field::getName).collect(Collectors.toList())
