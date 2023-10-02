7221c8ebc935ed67b8d1d835e47632a33ab5ef7b
peacetrue <xiayouxue@gmail.com>
2023-10-02 20:45:08 +0800

    支持 JDK 17

--- src/main/java/com/github/peacetrue/bean/BeanUtils.java
+++ src/main/java/com/github/peacetrue/bean/BeanUtils.java
@@ -137,6 +137,7 @@ public class BeanUtils {
      */
     public static Object invoke(Object object, Method method) {
         try {
+            method.setAccessible(true);
             return method.invoke(object);
         } catch (IllegalAccessException | InvocationTargetException e) {
             throw new IllegalStateException(e);
